e19fb16cad895515c7eb2ade92e2ac5a8efe07f3
Daan <daan.lambert@gmail.com>
2022-08-15 16:55:24 +0200

    made prover structure

---
+++ prover/prover.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDE_PROVER_
+#define INCLUDE_PROVER_
+
+# include <iostream>
+
+class Prover {
+  public:
+    Prover();
+    sendHash();
+    proveKnowledge();
+  private:
+    const int secret;
+}
+
+
+#endif
